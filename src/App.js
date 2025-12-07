aafc4f8b92a87977ac98f28b38687f15e9b86c66
yuyoungSong <syysyy0324@gmail.com>
2025-12-08 02:13:14 +0900

    Update App.js Route path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
@@ -6,9 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/hello" element={<h1>hello</h1>}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
