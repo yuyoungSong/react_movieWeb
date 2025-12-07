173a986cc992f30e03edfd069c8c9214865876a1
yuyoungSong <syysyy0324@gmail.com>
2025-12-08 01:56:42 +0900

    Update App.js Route path

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/hello" element={<h1>hello</h1>}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
